41ba1580762bb11351377ecb2ae8e5ae11ab78b5
Raman Gupta <7243222+raman325@users.noreply.github.com>
2020-04-20 22:41:10 -0400

    remove file.close since with statement auto-closes the file

--- utils.py
+++ utils.py
@@ -48,12 +48,10 @@ def return_data(path, default_data):
         write_data(path, default_data)
     with open(path,"r") as file:
         data = json.load(file)
-    file.close()
     return data
 def write_data(path,data):
     with open(path, "w") as file:
         json.dump(data, file)
-    file.close()
 def get_profile(profile_name):
     profiles = return_data("./data/profiles.json")
     for p in profiles:
