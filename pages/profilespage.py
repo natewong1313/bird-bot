98229681556467214c3aa52196b68eebd1db45e6
Raman Gupta <7243222+raman325@users.noreply.github.com>
2020-04-20 22:32:54 -0400

    remove default data files and generate them on demand

--- pages/profilespage.py
+++ pages/profilespage.py
@@ -270,7 +270,7 @@ class ProfilesPage(QtWidgets.QWidget):
             self.cardyear_box.addItem(str(year))
         for card_type in ["Visa", "Mastercard", "American Express", "Discover"]:
             self.cardtype_box.addItem(card_type)
-        profiles = return_data("./data/profiles.json")
+        profiles = return_data("./data/profiles.json", [])
         for profile in profiles:
             profile_name = profile["profile_name"]
             self.loadprofile_box.addItem(profile_name)
@@ -350,7 +350,7 @@ class ProfilesPage(QtWidgets.QWidget):
             "card_type": self.cardtype_box.currentText(),
             "card_cvv": self.cardcvv_edit.text()
         }      
-        profiles = return_data("./data/profiles.json")
+        profiles = return_data("./data/profiles.json", [])
         for p in profiles:
             if p["profile_name"] == profile_name:
                 profiles.remove(p)
@@ -364,7 +364,7 @@ class ProfilesPage(QtWidgets.QWidget):
     
     def delete_profile(self):
         profile_name = self.profilename_edit.text()
-        profiles = return_data("./data/profiles.json")
+        profiles = return_data("./data/profiles.json", [])
         for profile in profiles:
             if profile["profile_name"] == profile_name:
                 profiles.remove(profile)
